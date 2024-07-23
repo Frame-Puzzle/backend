70fe5eb825610ed197ccce813421481075d019ff
이석범 <dltjrqja01@naver.com>
2024-07-24 01:31:40 +0900

    [ADD][BE][석범] slf4j추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/service/UserService.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/service/UserService.java
@@ -3,10 +3,12 @@ package com.frazzle.main.domain.user.service;
 import com.frazzle.main.domain.user.entity.User;
 import com.frazzle.main.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
