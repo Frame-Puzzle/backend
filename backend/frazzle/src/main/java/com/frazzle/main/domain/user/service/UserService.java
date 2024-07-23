d01098f0c4b7378694199aeda0f6e6c279ba6ad1
이석범 <dltjrqja01@naver.com>
2024-07-23 17:41:02 +0900

    [FIX][BE][석범] JWT토큰 추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/service/UserService.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/service/UserService.java
@@ -2,8 +2,13 @@ package com.frazzle.main.domain.user.service;
 
 import com.frazzle.main.domain.user.entity.User;
 import com.frazzle.main.domain.user.repository.UserRepository;
+import com.frazzle.main.global.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -11,7 +16,13 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public void login(User user) {
+    private final JwtUtil jwtUtil;
+
+    public String login(User user) {
         userRepository.save(user);
+
+        String jwt = jwtUtil.createToken(user.getEmail());
+
+        return jwt;
     }
 }
