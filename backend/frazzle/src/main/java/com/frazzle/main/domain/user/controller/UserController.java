d202c445ca6bc846900f52da9ead9c5825d55992
이석범 <dltjrqja01@naver.com>
2024-07-24 01:33:46 +0900

    [UPDATE][BE][석범] import 변경

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/controller/UserController.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/controller/UserController.java
@@ -1,24 +1,14 @@
 package com.frazzle.main.domain.user.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.frazzle.main.domain.auth.utils.SecurityUtil;
+import com.frazzle.main.global.utils.SecurityUtil;
 import com.frazzle.main.domain.user.entity.User;
 import com.frazzle.main.domain.user.service.UserService;
 import com.frazzle.main.global.exception.CustomException;
 import com.frazzle.main.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
