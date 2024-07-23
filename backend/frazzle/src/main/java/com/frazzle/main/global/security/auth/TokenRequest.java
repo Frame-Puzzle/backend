b4a328ff430da2c0488346f30f107dcf1adb2f07
이석범 <dltjrqja01@naver.com>
2024-07-23 14:44:04 +0900

    [FEAT][BE][석범] Token 받을시 클래스 추가

---
+++ backend/frazzle/src/main/java/com/frazzle/main/global/security/auth/TokenRequest.java
@@ -0,0 +1,13 @@
+package com.frazzle.main.global.security.auth;
+
+public class TokenRequest {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
