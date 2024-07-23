131acaf5a893c1d578583726e7f4663406ad5066
홍경현 <hyeon21owo@gmail.com>
2024-07-23 13:40:57 +0900

    [FEAT][BE][경현] 디렉토리 Entity 클래스 추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/directory/entity/Directory.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/directory/entity/Directory.java
@@ -1,2 +1,51 @@
-package com.frazzle.main.domain.directory.entity;public class Directory {
+package com.frazzle.main.domain.directory.entity;
+
+import com.frazzle.main.domain.directory.dto.RequestCreateDirectoryDto;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "directories")
+public class Directory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "directory_id")
+    private int directoryId;
+
+    @Column(name = "modify_date", nullable = false)
+    private LocalDateTime modifyDate;
+
+    @Column(name = "directory_name", nullable = false, length = 32)
+    private String directoryName;
+
+    @Column(name = "category", nullable = false, length = 12)
+    private String category;
+
+    @Column(name = "people_number", nullable = false)
+    private int peopleNumber;
+
+    @Builder
+    private Directory(String directoryName, String category, int peopleNumber) {
+        this.directoryName = directoryName;
+        this.category = category;
+        this.peopleNumber = peopleNumber;
+        this.modifyDate = LocalDateTime.now();
+    }
+
+    public static Directory createDirectory(RequestCreateDirectoryDto requestDto) {
+        return Directory.builder()
+                .directoryName(requestDto.getDirectoryName())
+                .category(requestDto.getCategory())
+                .peopleNumber(1)
+                .build();
+    }
 }
