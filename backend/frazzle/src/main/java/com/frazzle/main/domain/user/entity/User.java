bf87d024cf17e94c30907c7c238a9b99a4fe45b9
이석범 <dltjrqja01@naver.com>
2024-07-23 17:40:35 +0900

    [FIX][BE][석범] userEntity getter 추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/entity/User.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/entity/User.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "users")
+@Getter
 public class User {
 
     @Id
