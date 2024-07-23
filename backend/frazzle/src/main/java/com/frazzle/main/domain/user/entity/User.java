bd2aa6056538b0a6730c7f98dd0fbc4be2b30ecf
이석범 <dltjrqja01@naver.com>
2024-07-23 14:43:44 +0900

    [FEAT][BE][석범] UserEntity 추가

---
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/entity/User.java
@@ -0,0 +1,35 @@
+package com.frazzle.main.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String nickname;
+
+    private String email;
+
+    private String socialType;
+
+    private String profileImg;
+
+    private String token;
+
+    private String deviceToken;
+
+    protected User() {}
+
+    public User(String email, String socialType, String token) {
+        this.email = email;
+        this.socialType = socialType;
+        this.token = token;
+    }
+}
