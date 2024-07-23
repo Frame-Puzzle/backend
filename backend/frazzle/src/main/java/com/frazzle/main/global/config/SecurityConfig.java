9493813a231fbd8e0ea9201849e9a0d86a2285c9
이석범 <dltjrqja01@naver.com>
2024-07-24 00:54:12 +0900

    [FIX][BE][석범] 보안 접근 권한 설정 변경

--- backend/frazzle/src/main/java/com/frazzle/main/global/config/SecurityConfig.java
+++ backend/frazzle/src/main/java/com/frazzle/main/global/config/SecurityConfig.java
@@ -1,42 +1,62 @@
 package com.frazzle.main.global.config;
 
-import com.frazzle.main.global.security.auth.CustomOAuth2UserService;
+import com.frazzle.main.domain.auth.enums.UserRole;
+import com.frazzle.main.domain.auth.filter.JwtFilter;
+import com.frazzle.main.domain.auth.service.JwtTokenService;
+import com.frazzle.main.domain.user.service.UserService;
+import com.frazzle.main.global.exception.ExceptionHandlerFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
-    //보안 설정 메서드
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrfConfigurer -> csrfConfigurer.disable()) //csrf 비활성화 -> rest api이므로
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/","/api/v1", "/login**", "/error**", "/users/**").permitAll()
-                                .anyRequest().authenticated()
-                ); //모든 사용자는 /, /login, /error 페이지 접근 허용
-//                .oauth2Login(oauth2Login ->
-//                        oauth2Login
-//                                .loginPage("/login") //로그인 페이지
-//                                .defaultSuccessUrl("/home") //로그인 후 페이지
-//                                .failureUrl("/login?error") //실패 페이지
-//                                .userInfoEndpoint(userInfoEndpoint ->
-//                                        userInfoEndpoint
-//                                                .userService(customOAuth2UserService())
-//                                ) //사용자 정보 처리 메서드
-//                );
+    private final JwtTokenService jwtTokenService;
+    private final UserService userService;
 
-        return http.build();
+    @Bean
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //http 보안 설정으로 특정 url에 접근 권한 제한
+    @Bean
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        return http.cors(withDefaults())
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        //모두 접근 허용
+                        .requestMatchers("/login/**", "/token/refresh").permitAll()
+                        //유저만 접근 허용
+                        .requestMatchers("/user/**").hasAuthority(UserRole.USER.getRole())
+                        .anyRequest().authenticated())
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable()) // 로그인 폼 미사용
+                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable()) // http basic 미사용
+                .addFilterBefore(new JwtFilter(jwtTokenService, userService), UsernamePasswordAuthenticationFilter.class) // JWT Filter 추가
+                .addFilterBefore(new ExceptionHandlerFilter(), JwtFilter.class) // JwtFilter 에서 CustomException 사용하기 위해 추가
+                .build();
     }
 
     @Bean
-    public CustomOAuth2UserService customOAuth2UserService() {
-        return new CustomOAuth2UserService();
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        // 아래 url은 filter 에서 제외
+        return web ->
+                web.ignoring()
+                        .requestMatchers("/login/**", "/token/refresh");
     }
 }
