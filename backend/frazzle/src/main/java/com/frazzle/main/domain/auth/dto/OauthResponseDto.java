8e20514c4d247a6fa06f00b2f199d8764cd712a4
이석범 <dltjrqja01@naver.com>
2024-07-24 13:17:02 +0900

    [UPDATE][BE][석범] 빌더 변경

--- backend/frazzle/src/main/java/com/frazzle/main/domain/auth/dto/OauthResponseDto.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/auth/dto/OauthResponseDto.java
@@ -1,10 +1,11 @@
 package com.frazzle.main.domain.auth.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OauthResponseDto {
     private String accessToken;
 }
