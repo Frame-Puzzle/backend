8e57a4da5243cbe51a4fa16cfe2e478ba3890435
이석범 <dltjrqja01@naver.com>
2024-07-24 16:32:20 +0900

    [FEAT][BE][석범] 구글 로그인 추가

---
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/auth/service/GoogleOauthService.java
@@ -0,0 +1,82 @@
+package com.frazzle.main.domain.auth.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.frazzle.main.domain.auth.dto.GoogleInfoDto;
+import com.frazzle.main.domain.auth.dto.KakaoInfoDto;
+import com.frazzle.main.domain.user.entity.User;
+import com.frazzle.main.domain.user.service.UserService;
+import com.frazzle.main.global.exception.CustomException;
+import com.frazzle.main.global.exception.ErrorCode;
+import com.frazzle.main.global.utils.GenerateRandomNickname;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GoogleOauthService implements SocialOauthService {
+
+    private final UserService userService;
+
+
+    //프론트에서 가져온 어세스 토큰을 이용해서 카카오에서 정보를 가져옴
+    @Override
+    public Map<String, Object> getUserAttributesByToken(String accessToken) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String userInfoUri = "https://www.googleapis.com/oauth2/v2/userinfo";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+
+        //바디와 헤더 설정
+        HttpEntity<String> entity = new HttpEntity<>("", headers);
+
+        //uri, 메서드, 헤더 또는 엔티티, 반환 타입
+        ResponseEntity<Map<String, Object>> response = restTemplate.exchange(userInfoUri, HttpMethod.GET, entity, new ParameterizedTypeReference<Map<String, Object>>() {
+        });
+
+        return response.getBody();
+    }
+
+
+    //가져온 accesstoken을 가공해서 email과 id만 찾아서 가져옴
+    @Override
+    public User getUserProfileByToken(String accessToken) {
+        Map<String, Object> userAttributesByToken = getUserAttributesByToken(accessToken);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = mapper.readTree(mapper.writeValueAsString(userAttributesByToken));
+        } catch (JsonProcessingException e) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        }
+
+        log.info("json", jsonNode.toString());
+
+        GoogleInfoDto googleInfoDto = new GoogleInfoDto(jsonNode);
+
+        log.info(googleInfoDto.toString());
+
+        User user = User.createUser(googleInfoDto.getId(), GenerateRandomNickname.generateRandomNickname(), googleInfoDto.getEmail(), "google");
+
+        //db에 존재하면 업데이트 아니면 insert
+        if(userService.findByLoginUserId(user.getLoginUserId()) !=null) {
+            userService.update(user);
+        }
+        else {
+            userService.save(user);
+        }
+        return user;
+    }
+}
