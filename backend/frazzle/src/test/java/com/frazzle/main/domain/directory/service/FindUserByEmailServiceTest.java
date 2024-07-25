70c1902a6aacd832e1b1c768abda057e776a4f38
홍경현 <hyeon21owo@gmail.com>
2024-07-25 16:55:52 +0900

    [FEAT][BE][경현] 이메일로 멤버 정보 조회 권한 없음 실패 테스트

--- backend/frazzle/src/test/java/com/frazzle/main/domain/directory/service/FindUserByEmailServiceTest.java
+++ backend/frazzle/src/test/java/com/frazzle/main/domain/directory/service/FindUserByEmailServiceTest.java
@@ -8,6 +8,8 @@ import com.frazzle.main.domain.user.entity.User;
 import com.frazzle.main.domain.user.repository.UserRepository;
 import com.frazzle.main.domain.userdirectory.entity.UserDirectory;
 import com.frazzle.main.domain.userdirectory.repository.UserDirectoryRepository;
+import com.frazzle.main.global.exception.CustomException;
+import com.frazzle.main.global.exception.ErrorCode;
 import com.frazzle.main.global.models.UserPrincipal;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -42,24 +44,23 @@ public class FindUserByEmailServiceTest {
 
     private String email;
     private Directory directory;
-    private UserDirectory userDirectory;
     private User user1;
     private User user2;
     private long loginUserId1;
+    private long loginUserId2;
     private List<User> users;
 
     @BeforeEach
     public void setup(){
         loginUserId1 = 1;
         user1 = User.createUser(loginUserId1,"김싸피", "ssafy@ssafy.com", "kakao");
-
-        user2 = User.createUser(2L,"SSafy", "ssafy1@ssafy.com", "kakao");
+        loginUserId2 = 2;
+        user2 = User.createUser(loginUserId2,"SSafy", "ssafy1@ssafy.com", "kakao");
 
         users = new ArrayList<>();
         users.add(user2);
 
         directory = Directory.createDirectory(new CreateDirectoryRequestDto("친구", "B208"));
-        userDirectory = UserDirectory.createUserDirectory(directory, user1, true);
 
         email = "s";
     }
@@ -79,5 +80,22 @@ public class FindUserByEmailServiceTest {
 
         //then
         Assertions.assertThat(responses.size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("이메일로 멤버 정보 조회 권한 없음 실패 테스트")
+    public void 이메일로_멤버_정보_조회_권한_없음_실패_테스트(){
+        //given
+        BDDMockito.given(userPrincipal.getId()).willReturn(loginUserId2);
+        BDDMockito.given(userRepository.findByLoginUserId(loginUserId2)).willReturn(user2);
+        BDDMockito.given(directoryRepository.findByDirectoryId(directory.getDirectoryId())).willReturn(directory);
+        BDDMockito.given(userDirectoryRepository.existsByDirectoryAndUserAndIsAccept(directory, user2, true)).willReturn(false);
+
+        Assertions.assertThatThrownBy(() -> directoryService.findUserByEmail(userPrincipal, email, directory.getDirectoryId()))
+                .isInstanceOf(CustomException.class)
+                .hasMessageContaining(ErrorCode.DENIED_FIND_MEMBER.getMessage());
+
+        //then
+        BDDMockito.then(userDirectoryRepository).should().existsByDirectoryAndUserAndIsAccept(directory, user2, true);
     }
 }
