88b21158ca4a382b5a13344eee1f12fd39398737
홍경현 <hyeon21owo@gmail.com>
2024-07-25 13:45:56 +0900

    [FEAT][BE][경현] 디렉토리 레포지토리 수정 (디렉토리 아이디로 디렉토리 찾기, 커스텀 레포지토리 extends)

--- backend/frazzle/src/main/java/com/frazzle/main/domain/directory/repository/DirectoryRepository.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/directory/repository/DirectoryRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DirectoryRepository extends JpaRepository<Directory, Integer> {
+public interface DirectoryRepository extends JpaRepository<Directory, Integer>, DirectoryRepositoryCustom {
+
+    Directory findByDirectoryId(int directoryId);
+
+    @Override
+    long updateNameByDirectoryId(int directoryId, String directoryName);
 }
