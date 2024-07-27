2b7d6392db7eb868c9c13522ccd683c820e5dded
이석범 <dltjrqja01@naver.com>
2024-07-27 21:40:41 +0900

    [UPDATE][BE][석범] 유저 정보 업데이트 추가 및 영속성 추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/repository/UserRepositoryImpl.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/repository/UserRepositoryImpl.java
@@ -1,13 +1,17 @@
 package com.frazzle.main.domain.user.repository;
 
 import com.frazzle.main.domain.directory.entity.Directory;
+import com.frazzle.main.domain.user.dto.UpdateUserRequestDto;
 import com.frazzle.main.domain.user.entity.QUser;
 import com.frazzle.main.domain.user.entity.User;
 import com.frazzle.main.domain.userdirectory.entity.QUserDirectory;
+import com.frazzle.main.domain.userdirectory.entity.UserDirectory;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,10 +19,12 @@ import java.util.List;
 public class UserRepositoryImpl implements UserRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+    private final EntityManager em;
 
     private QUser user = QUser.user;
     private QUserDirectory userDirectory = QUserDirectory.userDirectory;
 
+    //이메일로 찾기
     @Override
     public List<User> findUsersByEmail(String email, Directory directory) {
         JPQLQuery<User> subquery = JPAExpressions
@@ -32,5 +38,72 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                 .where(user.email.like(email + "%")
                         .and(user.notIn(subquery)))
                 .fetch();
+    }
+
+    @Override
+    @Transactional
+    public Long updateUser(User findUser ,User updateUser) {
+        //유저 정보를 이용해 업데이트
+        Long result = queryFactory
+                .update(user)
+                .where(user.eq(findUser))
+                .set(user.loginUserId, updateUser.getLoginUserId())
+                .set(user.email, updateUser.getEmail())
+                .set(user.refreshToken, updateUser.getRefreshToken())
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        return result;
+    }
+
+    @Override
+    @Transactional
+    //리프레시 토큰 업데이트
+    public Long updateRefreshToken(User updateUser, String refreshToken) {
+        Long result = queryFactory
+                .update(user)
+                .where(user.eq(updateUser))
+                .set(user.refreshToken, refreshToken)
+                .execute();
+
+        em.flush();
+        em.clear();
+
+
+        return result;
+    }
+
+    @Override
+    @Transactional
+    //닉네임 업데이트
+    public Long updateNickname(User updateUser, UpdateUserRequestDto requestDto) {
+        Long result = queryFactory
+                .update(user)
+                .where(user.eq(updateUser))
+                .set(user.nickname, requestDto.getNickname())
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        return result;
+    }
+
+    @Override
+    @Transactional
+    //프로필 업데이트
+    public Long updateProfileImg(User updateUser, UpdateUserRequestDto requestDto) {
+        Long result = queryFactory
+                .update(user)
+                .where(user.eq(updateUser))
+                .set(user.profileImg, requestDto.getProfileImg())
+                .execute();
+
+        em.flush();
+        em.clear();
+
+        return result;
     }
 }
