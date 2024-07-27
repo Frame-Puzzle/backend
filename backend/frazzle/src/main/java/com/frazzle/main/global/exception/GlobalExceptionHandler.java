2b63a141922371017019bd51e3b5b996d16bda25
이석범 <dltjrqja01@naver.com>
2024-07-27 21:46:24 +0900

    [UPDATE][BE][석범] resultDto로 변경

--- backend/frazzle/src/main/java/com/frazzle/main/global/exception/GlobalExceptionHandler.java
+++ backend/frazzle/src/main/java/com/frazzle/main/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.frazzle.main.global.exception;
 
+import com.frazzle.main.global.utils.ResultDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
