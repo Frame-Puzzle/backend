39e21b36bb696c07b80a0d84719c8406d7121a9c
이석범 <dltjrqja01@naver.com>
2024-07-29 00:10:20 +0900

    [FIX][BE][석범] 카카오 api 변경

--- backend/frazzle/src/main/java/com/frazzle/main/domain/auth/service/KakaoOauthService.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/auth/service/KakaoOauthService.java
@@ -11,14 +11,13 @@ import com.frazzle.main.global.exception.ErrorCode;
 import com.frazzle.main.global.utils.GenerateRandomNickname;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -28,23 +27,60 @@ public class KakaoOauthService implements SocialOauthService {
 
     private final UserService userService;
 
+    @Value("${kakao.client-id}")
+    String clientId;// 카카오 rest api 키
+    @Value("${kakao.redirect-uri}")
+    String redirectUri; // 카카오에 등록한 redirect_uri
+
     //프론트에서 가져온 access 토큰을 이용해서 카카오에서 정보를 가져옴
     @Override
-    public Map<String, Object> getUserAttributesByToken(String accessToken) {
+    public Map<String, Object> getUserAttributesByToken(String code) {
         RestTemplate restTemplate = new RestTemplate();
+        String getKakaoAccessTokenUri = "https://kauth.kakao.com/oauth/token";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("grant_type","authorization_code");
+        body.put("client_id",clientId);
+        body.put("redirect_uri",redirectUri);
+        body.put("code", code);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(getKakaoAccessTokenUri, body, String.class);
+
+        String responseBody = response.getBody();
+
+        String accessToken = null;
+
+        try {
+            // JSON 응답 파싱
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(responseBody);
+
+            // access_token 추출
+            accessToken = rootNode.path("access_token").asText();
+
+            log.info(accessToken);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        restTemplate = new RestTemplate();
 
         String userInfoUri = "https://kapi.kakao.com/v2/user/me";
-        HttpHeaders headers = new HttpHeaders();
+        headers = new HttpHeaders();
         headers.setBearerAuth(accessToken);
 
         //바디와 헤더 설정
         HttpEntity<String> entity = new HttpEntity<>("", headers);
 
         //uri, 메서드, 헤더 또는 엔티티, 반환 타입
-        ResponseEntity<Map<String, Object>> response = restTemplate.exchange(userInfoUri, HttpMethod.GET, entity, new ParameterizedTypeReference<Map<String, Object>>() {
+        ResponseEntity<Map<String, Object>> accessTokenResponse = restTemplate.exchange(userInfoUri, HttpMethod.GET, entity, new ParameterizedTypeReference<Map<String, Object>>() {
         });
 
-        return response.getBody();
+        return accessTokenResponse.getBody();
     }
 
 
