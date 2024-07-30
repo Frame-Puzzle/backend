ec0c774fa046cf157a2b3da925a938efc94b52b4
이석범 <dltjrqja01@naver.com>
2024-07-30 17:21:23 +0900

    [FEAT][BE][석범] aws서비스 추가

---
+++ backend/frazzle/src/main/java/com/frazzle/main/global/aws/service/AwsService.java
@@ -0,0 +1,69 @@
+package com.frazzle.main.global.aws.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.frazzle.main.global.exception.CustomException;
+import com.frazzle.main.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.net.URL;
+import java.util.UUID;
+
+import static com.frazzle.main.global.utils.FileUtil.convertMultiPartFileToFile;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class AwsService {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String name;
+
+    private final AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file, String loginUserId) {
+        try {
+            File fileObj = convertMultiPartFileToFile(file);
+
+            String originalFilename = file.getOriginalFilename();
+
+            String extension = "";
+            int dotIndex = originalFilename.lastIndexOf('.');
+            if (dotIndex > 0) {
+                extension = originalFilename.substring(dotIndex);
+            }
+
+            String uniqueFileName = loginUserId + extension;
+
+            s3Client.putObject(new PutObjectRequest(name, uniqueFileName, fileObj));
+            fileObj.delete();
+
+
+
+            return extension;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new CustomException(ErrorCode.FAILED_CONVERT_FILE);
+        }
+    }
+
+    public String getProfileUri(String loginUserId) {
+        URL url = s3Client.getUrl(name, loginUserId);
+        return ""+url;
+    }
+
+//    private String generateUniqueFileName(String loginUserId, String originalFilename) {
+//        String extension = "";
+//        int dotIndex = originalFilename.lastIndexOf('.');
+//        if (dotIndex > 0) {
+//            extension = originalFilename.substring(dotIndex);
+//        }
+//        return loginUserId + extension;
+//    }
+
+}
