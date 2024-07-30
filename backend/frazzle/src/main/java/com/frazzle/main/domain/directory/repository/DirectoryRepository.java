7f8616653efe562c25536e14e781e4587fa8ec27
홍경현 <hyeon21owo@gmail.com>
2024-07-30 15:05:58 +0900

    [FEAT][BE][경현] 디렉토리 찾기 Custom Repository 오버라이드

--- backend/frazzle/src/main/java/com/frazzle/main/domain/directory/repository/DirectoryRepository.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/directory/repository/DirectoryRepository.java
@@ -1,9 +1,11 @@
 package com.frazzle.main.domain.directory.repository;
 
 import com.frazzle.main.domain.directory.entity.Directory;
+import com.frazzle.main.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +14,5 @@ public interface DirectoryRepository extends JpaRepository<Directory, Integer>, 
     Optional<Directory> findByDirectoryId(int directoryId);
 
     @Override
-    long updateNameByDirectoryId(int directoryId, String directoryName);
+    List<Directory> findMyDirectory(User user, String category);
 }
