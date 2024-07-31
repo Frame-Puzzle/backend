b26be88e8e6cbacaccf7b95ed37324dbe8104c74
이석범 <dltjrqja01@naver.com>
2024-07-31 09:55:23 +0900

    [FEAT][BE][석범] 알림 컨트롤러 추가

---
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/notification/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package com.frazzle.main.domain.notification.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@CrossOrigin(origins = "*")
+@RequestMapping("/notifications")
+public class NotificationController {
+}
