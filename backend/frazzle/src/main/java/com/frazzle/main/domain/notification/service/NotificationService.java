1171c1b866b1f395d901c1ff0e9ea55a654439a9
이석범 <dltjrqja01@naver.com>
2024-07-31 23:10:14 +0900

    [FEAT][BE][석범] 유저알림 읽기 여부 추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/notification/service/NotificationService.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/notification/service/NotificationService.java
@@ -1,8 +1,11 @@
 package com.frazzle.main.domain.notification.service;
 
+import com.frazzle.main.domain.notification.dto.AcceptNotificationRequestDto;
 import com.frazzle.main.domain.notification.entity.Notification;
 import com.frazzle.main.domain.notification.repository.NotificationRepository;
 import com.frazzle.main.domain.user.entity.User;
+import com.frazzle.main.domain.usernotification.entity.UserNotification;
+import com.frazzle.main.domain.usernotification.repository.UserNotificationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +15,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+    private final UserNotificationRepository userNotificationRepository;
 
     public List<Notification> findAllByUser(User user) {
         return notificationRepository.findByUser(user);
+    }
+
+    public void updateUserNotification(User user, int notificationId, AcceptNotificationRequestDto requestDto) {
+        Notification notification = notificationRepository.findByNotificationId(notificationId);
+
+        UserNotification userNotification = userNotificationRepository.findByUserAndNotification(user, notification);
+
+        userNotification.updateRead();
+
+        userNotification.updateStatus(requestDto.getAccept());
+
     }
 }
