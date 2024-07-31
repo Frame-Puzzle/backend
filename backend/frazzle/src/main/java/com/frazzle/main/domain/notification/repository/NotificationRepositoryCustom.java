f4e4636d14f28f51c63a1e9d43245ec27a6c2628
이석범 <dltjrqja01@naver.com>
2024-07-31 12:40:55 +0900

    [FEAT][BE][석범] 유저 알림 추가

---
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/notification/repository/NotificationRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.frazzle.main.domain.notification.repository;
+
+import com.frazzle.main.domain.notification.entity.Notification;
+import com.frazzle.main.domain.user.entity.User;
+
+import java.util.List;
+
+public interface NotificationRepositoryCustom {
+    List<Notification> findByUser(User user);
+}
