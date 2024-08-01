a833cd751df76dabf13d96a8edef869a02efb4bd
이석범 <dltjrqja01@naver.com>
2024-08-01 14:30:32 +0900

    [REFACTOR][BE][석범] 파일로 변경

--- backend/frazzle/src/main/java/com/frazzle/main/global/utils/FindPeopleCountFromImg.java
+++ backend/frazzle/src/main/java/com/frazzle/main/global/utils/FindPeopleCountFromImg.java
@@ -1,7 +1,12 @@
 package com.frazzle.main.global.utils;
+
+import com.frazzle.main.global.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,38 +14,36 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Base64;
 
+@Slf4j
+@Component
 public class FindPeopleCountFromImg {
 
     @Value("${google.vision.api}")
-    private static String apiKey;
+    private String apiKey;
 
-    //사진 url 입력받는 메서드 
-    public static int inputImgUrl(String imgUrl) {
-        //사진 경로
-        Path path = Path.of(imgUrl);
-        
-        //사람 최대 인원 수
+    // MultipartFile을 입력받는 메서드
+    public int analyzeImageFile(MultipartFile image) {
+        log.info("analyzeImageFile");
+        // 사람 최대 인원 수
         int maxResults = 10;
 
         try {
-            String response = detectFaces(path, maxResults, apiKey);
+            String response = detectFaces(image, maxResults, apiKey);
             int faceCount = countFaces(response);
-            
+
+            log.info("Face count: " + faceCount);
             return faceCount;
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(ErrorCode.FAILED_CONVERT_FILE.getMessage());
         }
-        
-        return 0;
     }
 
-    /** 주어진 경로에 있는 이미지에서 얼굴을 감지하고 응답을 문자열로 반환합니다. */
-    public static String detectFaces(Path path, int maxResults, String apiKey) throws IOException {
-        byte[] data = Files.readAllBytes(path);
+    /** 주어진 MultipartFile에서 얼굴을 감지하고 응답을 문자열로 반환합니다. */
+    private String detectFaces(MultipartFile file, int maxResults, String apiKey) throws IOException {
+        log.info("detectFaces");
+        byte[] data = file.getBytes();
         String base64Image = Base64.getEncoder().encodeToString(data);
 
         // JSON 요청 본문 구성
@@ -84,8 +87,9 @@ public class FindPeopleCountFromImg {
         return response.toString();
     }
 
-    /** 응답에서 감지된 얼굴 수를 계산합니다 */
-    public static int countFaces(String jsonResponse) {
+    /** 응답에서 감지된 얼굴 수를 계산합니다. */
+    private int countFaces(String jsonResponse) {
+        log.info("countFaces");
         JSONObject responseJson = new JSONObject(jsonResponse);
         JSONArray responses = responseJson.getJSONArray("responses");
         if (responses.length() > 0) {
