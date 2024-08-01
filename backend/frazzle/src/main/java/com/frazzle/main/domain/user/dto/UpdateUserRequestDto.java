fc99d30403e464c6c829c27ed087645cdd88abf9
이석범 <dltjrqja01@naver.com>
2024-08-01 12:53:11 +0900

    [DEL][BE][석범] 안쓰는 import 제거

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/dto/UpdateUserRequestDto.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/dto/UpdateUserRequestDto.java
@@ -1,12 +1,4 @@
 package com.frazzle.main.domain.user.dto;
 
-import com.frazzle.main.domain.user.validation.annotation.AtLeastOneNotBlank;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
-
 public interface UpdateUserRequestDto {
 }
