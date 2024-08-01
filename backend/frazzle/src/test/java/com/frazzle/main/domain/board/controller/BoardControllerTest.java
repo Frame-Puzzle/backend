d88e288d1930702e265cc43ee199b6fdcada8916
현준호 <zlzl9432@naver.com>
2024-08-02 06:09:12 +0900

    [UPDATE][BE][준호] Test Class 생성

---
+++ backend/frazzle/src/test/java/com/frazzle/main/domain/board/controller/BoardControllerTest.java
@@ -0,0 +1,28 @@
+package com.frazzle.main.domain.board.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardControllerTest {
+
+    @Test
+    void createBoard() {
+    }
+
+    @Test
+    void findBoardAndPiece() {
+    }
+
+    @Test
+    void deleteVote() {
+    }
+
+    @Test
+    void getBoardImages() {
+    }
+
+    @Test
+    void updateBoardThumbnails() {
+    }
+}
---
+++ backend/frazzle/src/test/java/com/frazzle/main/domain/piece/controller/PieceControllerTest.java
@@ -0,0 +1,21 @@
+package com.frazzle.main.domain.piece.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceControllerTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void updatePiece() {
+    }
+
+    @Test
+    void detailPiece() {
+    }
+}
