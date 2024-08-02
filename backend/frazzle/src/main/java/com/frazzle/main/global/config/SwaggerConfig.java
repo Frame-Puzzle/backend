3937bf9b346ced501a5b794086b5f16845531bcb
이석범 <dltjrqja01@naver.com>
2024-08-02 09:30:01 +0900

    [UPDATE][BE][석범] 스웨거 업데이트

--- backend/frazzle/src/main/java/com/frazzle/main/global/config/SwaggerConfig.java
+++ backend/frazzle/src/main/java/com/frazzle/main/global/config/SwaggerConfig.java
@@ -1,11 +1,11 @@
 package com.frazzle.main.global.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,15 +16,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        //bearer-token을 가지는 보안 스키마 추가
-
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearer-key",
-                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
                 .info(new Info().title("Frazzle API")
                         .description("Frazzle API 문서")
                         .version("1.0"))
-                        .servers(List.of(new Server().url("https://i11b208.p.ssafy.io")));
+                .servers(Arrays.asList(new Server().url("https://i11b208.p.ssafy.io/api/v1")));  // 서버 URL을 HTTPS로 설정 및 기본 경로 추가
     }
 }
