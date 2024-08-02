16f780ec03bd98b31fe91701aaafd483b1c914cb
이석범 <dltjrqja01@naver.com>
2024-08-02 13:58:12 +0900

    [FEAT][BE][석범] 응답값 변경

--- backend/frazzle/src/main/java/com/frazzle/main/global/openai/controller/GuideController.java
+++ backend/frazzle/src/main/java/com/frazzle/main/global/openai/controller/GuideController.java
@@ -3,8 +3,11 @@ package com.frazzle.main.global.openai.controller;
 import com.frazzle.main.global.openai.dto.GuideRequestDto;
 import com.frazzle.main.global.openai.dto.GuideResponseDto;
 import com.frazzle.main.global.openai.service.GuideService;
+import com.frazzle.main.global.utils.ResultDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +24,13 @@ public class GuideController {
     private final GuideService gptService;
 
     //추후에 퍼즐판 생성으로 넘어감
+    //보드id로
     @PostMapping("/guides")
-    public GuideResponseDto describeImage(@RequestBody GuideRequestDto requestDto) {
+    public ResponseEntity<ResultDto> describeImage(@RequestBody GuideRequestDto requestDto) {
 
         String[] guideList = gptService.generateDescription(requestDto);
 
-        return GuideResponseDto.createGuideResponseDto(guideList);
+        return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "", guideList));
+
     }
 }
