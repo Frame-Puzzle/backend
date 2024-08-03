8d540ea01570527152b730123e7ca24a12929154
홍경현 <hyeon21owo@gmail.com>
2024-08-04 00:08:59 +0900

    [FEAT][BE][경현] 디렉토리 상세 조회 response dto 생성

---
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/directory/dto/DetailDirectoryResponsetDto.java
@@ -0,0 +1,39 @@
+package com.frazzle.main.domain.directory.dto;
+
+import com.frazzle.main.domain.directory.entity.Directory;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DetailDirectoryResponsetDto {
+
+    private int directoryId;
+    private String directoryName;
+    private boolean isCurrentBoard;
+    private List<MemberListDto> memberList;
+    private List<BoardListDto> boardList;
+
+    @Builder
+    private DetailDirectoryResponsetDto(int directoryId, String directoryName, boolean isCurrentBoard, List<MemberListDto> memberList, List<BoardListDto> boardList) {
+        this.directoryId = directoryId;
+        this.directoryName = directoryName;
+        this.isCurrentBoard = isCurrentBoard;
+        this.memberList = memberList;
+        this.boardList = boardList;
+    }
+
+    public static DetailDirectoryResponsetDto createDetailDirectoryRequestDto(Directory directory, boolean isCurrentBoard, List<MemberListDto> memberList, List<BoardListDto> boardList) {
+        return DetailDirectoryResponsetDto.builder()
+                .directoryId(directory.getDirectoryId())
+                .directoryName(directory.getDirectoryName())
+                .isCurrentBoard(isCurrentBoard)
+                .memberList(memberList)
+                .boardList(boardList)
+                .build();
+    }
+}
