d29fbe24c055865adb0ef5041e14f7b2b7d6972a
홍경현 <hyeon21owo@gmail.com>
2024-08-04 00:11:27 +0900

    [FEAT][BE][경현] QBoard @bean 등록 -> 왜 자동 등록이 안되는지 모르겠어요 추후 수정예정

--- backend/frazzle/src/main/java/com/frazzle/main/global/config/QueryDslFactoryConfig.java
+++ backend/frazzle/src/main/java/com/frazzle/main/global/config/QueryDslFactoryConfig.java
@@ -1,5 +1,6 @@
 package com.frazzle.main.global.config;
 
+import com.frazzle.main.domain.board.entity.QBoard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +13,10 @@ public class QueryDslFactoryConfig {
     public JPAQueryFactory jpaQueryFactory(EntityManager em){
 
         return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    public QBoard qBoard() {
+        return new QBoard("board");
     }
 }
