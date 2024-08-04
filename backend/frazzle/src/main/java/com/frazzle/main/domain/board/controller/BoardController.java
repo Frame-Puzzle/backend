429e0ca2ad511e8624ac2b8446297c201b0797aa
이석범 <dltjrqja01@naver.com>
2024-08-04 14:57:22 +0900

    [DOCS][BE][석범] 스웨거 추가

--- backend/frazzle/src/main/java/com/frazzle/main/domain/board/controller/BoardController.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/board/controller/BoardController.java
@@ -5,6 +5,11 @@ import com.frazzle.main.domain.board.entity.Board;
 import com.frazzle.main.domain.board.service.BoardService;
 import com.frazzle.main.global.models.UserPrincipal;
 import com.frazzle.main.global.utils.ResultDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,6 +26,13 @@ public class BoardController {
     private final BoardService boardService;
 
     //퍼즐판 및 퍼즐조각 전체 조회
+    @Operation(summary = "퍼즐판전체 조회", description = "퍼즐판과 퍼즐판 내의 퍼즐조각의 관한 정보 전체 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "조회에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @GetMapping("/{boardID}")
     public ResponseEntity<ResultDto> findBoardAndPiece(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -34,6 +46,13 @@ public class BoardController {
     }
 
     //투표
+    @Operation(summary = "퍼즐판삭제 투표하기", description = "퍼즐판 삭제를 위한 투표하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "투표에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "투표에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @PutMapping("/{boardID}/vote")
     public ResponseEntity<ResultDto> deleteVote(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -48,6 +67,13 @@ public class BoardController {
     }
 
     //퍼즐판 내 전체 사진 조회
+    @Operation(summary = "퍼즐판 내 전체 사진 조회하기", description = "완료 퍼즐판에서 전체 사진을 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "조회에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @GetMapping("/{boardID}/images")
     public ResponseEntity<ResultDto> getBoardImages(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -63,6 +89,13 @@ public class BoardController {
 
     //썸네일 생성 및 수정
     @PutMapping("/{boardID}/thumbnails")
+    @Operation(summary = "퍼즐판 쎔네일 사진 수정하기", description = "완료 퍼즐판에서 쎔네일 사진을 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "수정에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     public ResponseEntity<ResultDto> updateBoardThumbnails(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
             @PathVariable("boardID") int boardID,
--- backend/frazzle/src/main/java/com/frazzle/main/domain/directory/controller/DirectoryController.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/directory/controller/DirectoryController.java
@@ -8,6 +8,11 @@ import com.frazzle.main.domain.directory.entity.Directory;
 import com.frazzle.main.domain.directory.service.DirectoryService;
 import com.frazzle.main.global.models.UserPrincipal;
 import com.frazzle.main.global.utils.ResultDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +36,13 @@ public class DirectoryController {
     private final DirectoryService directoryService;
     private final BoardService boardService;
 
+    @Operation(summary = "디렉토리 생성", description = "디렉토리 생성하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "생성에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "생성에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @PostMapping
     public ResponseEntity<?> createDirectory(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -42,6 +54,13 @@ public class DirectoryController {
         return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "디렉토리 등록에 성공했습니다.", responseDto));
     }
 
+    @Operation(summary = "디렉토리명 수정", description = "디렉토리명 수정하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "수정에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "수정에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @PutMapping("/{directoryId}")
     public ResponseEntity<?> updateDirectoryName(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -52,6 +71,13 @@ public class DirectoryController {
         return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "디렉토리 이름을 수정하였습니다."));
     }
 
+    @Operation(summary = "디렉토리 초대 멤버 여부 조회", description = "디렉토리에 멤버 초대 여부 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "조회에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @GetMapping("/{directoryId}/users/find")
     public ResponseEntity<?> findUserByEmail(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -63,6 +89,13 @@ public class DirectoryController {
         return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "조회에 성공했습니다", data));
     }
 
+    @Operation(summary = "디렉토리의 멤버 초대", description = "디렉토리 내의 멤버 초대하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "초대에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "초대에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @PostMapping("/{directoryId}/user")
     public ResponseEntity<?> inviteMember(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -73,6 +106,13 @@ public class DirectoryController {
         return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "초대에 성공했습니다."));
     }
 
+    @Operation(summary = "디렉토리의 멤버 초대 취소", description = "디렉토리의 멤버 초대 취소하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "생성에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "생성에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @DeleteMapping("/{directoryId}/user")
     public ResponseEntity<?> cancelMemberInvitation(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -83,6 +123,13 @@ public class DirectoryController {
         return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "초대 취소에 성공했습니다."));
     }
 
+    @Operation(summary = "내가 참여한 디렉토리 찾기", description = "내가 참여한 디렉토리 찾기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "조회에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @GetMapping("")
     public ResponseEntity<?> findMyDirectory(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -94,6 +141,13 @@ public class DirectoryController {
         return ResponseEntity.status(HttpStatus.OK).body(ResultDto.res(HttpStatus.OK.value(), "조회에 성공했습니다.", data));
     }
 
+    @Operation(summary = "퍼즐판 생성", description = "퍼즐판 생성하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "생성에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "생성에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @PostMapping("/{directoryId}/boards")
     public ResponseEntity<ResultDto> createBoard(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
@@ -106,6 +160,13 @@ public class DirectoryController {
                         "퍼즐판 생성 성공", responseDto));
     }
 
+    @Operation(summary = "디렉토리 세부 조회", description = "디렉토리 세부 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "조회에 성공했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class))),
+            @ApiResponse(responseCode = "404", description = "조회에 실패했습니다.",
+                    content = @Content(schema = @Schema(implementation = ResultDto.class)))
+    })
     @GetMapping("{directoryId}")
     public ResponseEntity<?> findDetailDirectory(
             @AuthenticationPrincipal UserPrincipal userPrincipal,
