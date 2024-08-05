73757f45a4c028e658fc11524606344029e70a71
홍경현 <hyeon21owo@gmail.com>
2024-08-06 00:55:06 +0900

    [REFACTOR][BE][경현] 알림 삭제 로직을 알림 리스트에서 디렉토리 컬럼으로 변경

--- backend/frazzle/src/main/java/com/frazzle/main/domain/notification/repository/NotificationRepository.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/notification/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.frazzle.main.domain.notification.repository;
 
 
+import com.frazzle.main.domain.directory.entity.Directory;
 import com.frazzle.main.domain.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,5 @@ import java.util.List;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer>, NotificationRepositoryCustom {
     Notification findByNotificationId(int notificationId);
-    List<Notification> findByDirectory_DirectoryId(int directoryId);
-
-    @Override
-    long deleteByNotification(List<Notification> notifications);
+    void deleteAllByDirectory(Directory directory);
 }
