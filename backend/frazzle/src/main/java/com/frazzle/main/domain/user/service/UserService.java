be68c842f24c272c56f6ec1b4f1709dfb81d008d
이석범 <dltjrqja01@naver.com>
2024-08-06 15:40:32 +0900

    [FIX][BE][석범] 유저알림 모두 삭제

--- backend/frazzle/src/main/java/com/frazzle/main/domain/user/service/UserService.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/user/service/UserService.java
@@ -122,7 +122,12 @@ public class UserService {
             directoryService.leaveDirectory(userPrincipal, id);
         }
 
-        //4. 유저 디렉토리 삭제
+        List<UserNotification> userNotificationList = userNotificationRepository.findByUser(user);
+        if(!userNotificationList.isEmpty()) {
+            userNotificationRepository.deleteAll(userNotificationList);
+        }
+
+            //4. 유저 디렉토리 삭제
         userDirectoryRepository.deleteByUser(user);
 
         //4. 유저 삭제
