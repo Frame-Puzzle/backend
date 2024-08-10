7c359a69767286ac92b3b7e1ff0f8ff8e5e7e85d
이석범 <dltjrqja01@naver.com>
2024-08-10 09:59:34 +0900

    [FEAT][BE][석범] 시작시 서울 시간으로 하기

--- backend/frazzle/src/main/java/com/frazzle/main/FrazzleApplication.java
+++ backend/frazzle/src/main/java/com/frazzle/main/FrazzleApplication.java
@@ -1,13 +1,21 @@
 package com.frazzle.main;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class FrazzleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FrazzleApplication.class, args);
+    }
+
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
