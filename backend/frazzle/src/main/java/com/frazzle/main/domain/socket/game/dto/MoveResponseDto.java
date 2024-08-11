40440b64f55613cc0bbe44474ccf608499d552d9
이석범 <dltjrqja01@naver.com>
2024-08-11 21:30:55 +0900

    [FEAT][BE][석범] 게임 시작시 퍼즐 인덱스 랜덤 배치

--- backend/frazzle/src/main/java/com/frazzle/main/domain/socket/game/dto/MoveResponseDto.java
+++ backend/frazzle/src/main/java/com/frazzle/main/domain/socket/game/dto/MoveResponseDto.java
@@ -1,27 +1,33 @@
 package com.frazzle.main.domain.socket.game.dto;
 
-import lombok.*;
+import com.frazzle.main.domain.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
 public class MoveResponseDto {
-    private int index;
-    private float x;
-    private float y;
+    private int idx;
+    private String nickname;
+    private int userId;
+
 
     @Builder
-    private MoveResponseDto(int index, float x, float y) {
-        this.index = index;
-        this.x = x;
-        this.y = y;
+    private MoveResponseDto(int idx, String nickname, int userId) {
+        this.idx = idx;
+        this.nickname = nickname;
+        this.userId = userId;
     }
 
-    public static MoveResponseDto createResponseDto(int index, float x, float y) {
+
+
+    public static MoveResponseDto createResponseDto(int idx, User user) {
         return MoveResponseDto.builder()
-                .index(index)
-                .x(x)
-                .y(y)
+                .idx(idx)
+                .nickname(user.getNickname())
+                .userId(user.getUserId())
                 .build();
     }
 }
